a7bcb00d6e1f9fdb6c1f53fe31ec1111181412b7
Michal Marhan <marhanmichal@gmail.com>
2023-04-24 14:16:20 +0200

    Split APNS functions.

--- backend/apns.go
+++ backend/apns.go
@@ -9,7 +9,7 @@ import (
 	"github.com/sideshow/apns2"
 )
 
-func notify() {
+func CreateAPNSClient() *apns2.Client {
 	authKey, err := token.AuthKeyFromFile(os.Getenv("STORAGE_PATH") + "/authkey.p8")
 	if err != nil {
 		log.Fatal("token error:", err)
@@ -23,16 +23,20 @@ func notify() {
 		TeamID: "FBWNLT2RLU",
 	}
 
-	notification := &apns2.Notification{}
-	notification.DeviceToken = "e04914e1d28e58f01c8af573d46a5c2a492f52b453e5fa71b2e1b7ebde5f9c62"
-	notification.Topic = "com.marstad.freshpoint"
-	notification.Payload = []byte(`{"aps":{"alert":"Ahooj️ ☀️"}}`) // See Payload section below
-
 	// If you want to test push notifications for builds running directly from XCode (Development), use
 	// client := apns2.NewClient(cert).Development()
 	// For apps published to the app store or installed as an ad-hoc distribution use Production()
 
 	client := apns2.NewTokenClient(token).Development()
+	return client
+}
+
+func notify(client *apns2.Client) {
+	notification := &apns2.Notification{}
+	notification.DeviceToken = "e04914e1d28e58f01c8af573d46a5c2a492f52b453e5fa71b2e1b7ebde5f9c62"
+	notification.Topic = "com.marstad.freshpoint"
+	notification.Payload = []byte(`{"aps":{"alert":"Ahooj️ ☀️"}}`) // See Payload section below
+	
 	res, err := client.Push(notification)
 
 	if err != nil {
@@ -40,4 +44,5 @@ func notify() {
 	}
 
 	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+
 }
--- backend/main.go
+++ backend/main.go
@@ -3,24 +3,28 @@ package main
 import (
 	"freshpoint/backend/database"
 	"freshpoint/backend/freshpoint"
+	"github.com/sideshow/apns2"
 	"time"
 )
 
 var c *cache
 var d *database.Database
+var apns *apns2.Client
 
 func main() {
 	c = newCache()
 	d = database.NewConnection()
-	notify()
+	apns = CreateAPNSClient()
+
+	notify(apns)
 	// Run an auto-update goroutine for "my_data"
 	go c.SetAutoUpdate("freshpoint", 120*time.Second, func() interface{} {
 		println("Updating freshpoint cache")
 		curr := freshpoint.FetchProducts()
 		old, ok := c.Get("freshpoint")
 		if ok {
-			new := freshpoint.GetNewProducts(old.(freshpoint.FreshPointCatalog).Products, curr.Products)
-			print(new)
+			newProducts := freshpoint.GetNewProducts(old.(freshpoint.FreshPointCatalog).Products, curr.Products)
+			print(newProducts)
 		}
 		return curr
 	})
