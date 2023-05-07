525227200375d03e0a576a44d3925b55f761a583
Michal Marhan <marhanmichal@gmail.com>
2023-05-07 23:11:39 +0200

    Tab Bar with scrollspy

--- ios/freshpoint/freshpoint/Scenes/FoodList/FoodListView.swift
+++ ios/freshpoint/freshpoint/Scenes/FoodList/FoodListView.swift
@@ -10,22 +10,25 @@ import SwiftUI
 struct FoodListView: View {
     @StateObject var viewModel = FoodListViewModel()
     @Environment(\.scenePhase) var scenePhase
+    @State private var currentCategory: String = "Nápoje"
     
-    @State private var currentCategory: String = "Produkty"
     var body: some View {
-            ZStack{
-                switch viewModel.state {
-                case .loading, .failed, .initial:
-                    ProgressView()
-                case .fetched:
-                    makeList(from: viewModel.catalog.categories)
-                }
+        ZStack {
+            switch viewModel.state {
+            case .loading, .failed, .initial:
+                ProgressView()
+            case .fetched:
+                makeList(from: viewModel.catalog.categories)
+                    .overlay(alignment: .top) {
+                        makeHeader(from: viewModel.catalog.categories)
+                    }
             }
-            .overlay(alignment: .top, content: {
-                Color.clear // Or any view or color
-            .background(.regularMaterial) // I put clear here because I prefer to put a blur in this case. This modifier and the material it contains are optional.
-            .edgesIgnoringSafeArea(.top)
-            .frame(height: 0) // This will constrain the overlay to only go above the top safe area and not under.
+        }
+        .overlay(alignment: .top, content: {
+            Color.clear // Or any view or color
+                .background(.regularMaterial) // I put clear here because I prefer to put a blur in this case. This modifier and the material it contains are optional.
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 0) // This will constrain the overlay to only go above the top safe area and not under.
         })
         .task {
             await viewModel.fetch()
@@ -38,30 +41,50 @@ struct FoodListView: View {
         }
     }
     
-    func makeList(from categories: [CategoryItem]) -> some View {
-        List(categories) { category in
-            Section(header: HStack {
-                Section(header: ScrollView {
-                    HStack {
-                        ForEach(categories) { category in
-                            Text(category.name)
-                        }
+    func makeHeader(from categories: [CategoryItem]) -> some View {
+        ScrollViewReader { reader in
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(categories) { category in
                         Text(category.name)
+                            .fontWeight(category.name == currentCategory ? .bold : .regular)
+                            .foregroundColor(category.name == currentCategory ? .accentColor : .primary)
                             .padding()
+                            .tag(category.name)
                     }
                 }
-                Text(category.name)
-                    .padding()
-                Spacer()
-            }
                 .background(Color.clear)
                 .listRowInsets(EdgeInsets(
                     top: 0,
                     leading: 0,
                     bottom: 0,
                     trailing: 0))
-            )
-            {
+            }
+            .background(.thinMaterial)
+            .onChange(of: currentCategory) { newCategory in
+                withAnimation(.easeInOut) {
+                    reader.scrollTo(newCategory)
+                    
+                }
+            }
+        }
+        
+    }
+    
+    func makeList(from categories: [CategoryItem]) -> some View {
+        List(categories) { category in
+            VStack(alignment: .leading) {
+                GeometryReader { geometry in
+                    EmptyView().onChange(of: geometry.frame(in: .global)) { globalFrame in
+                        let offset = globalFrame.minY
+                        print("\(category.name): \(offset)")
+                        let height = UIApplication.shared.windows.first!.safeAreaInsets.top + 100
+                        if offset < height {
+                            currentCategory = category.name
+                        }
+                    }
+                }.frame(width: 0,height: 0)
+                Text(category.name).padding().font(.title)
                 ForEach(category.products) { item in
                     HStack {
                         AsyncImage(url: item.imageURL) { image in
@@ -78,10 +101,12 @@ struct FoodListView: View {
                             Text("Kusů: \(item.quantity)")
                                 .foregroundColor(.gray)
                         }
-                    }.listRowSeparator(.hidden)
+                    }
+                    .listRowSeparator(.hidden)
                 }
-                
-            }.headerProminence(.increased)
+                .tag(category.name)
+            }
+            
         }
         .listStyle(.plain)
         .scrollContentBackground(.hidden)
