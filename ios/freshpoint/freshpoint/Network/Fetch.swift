9ee4eed4c3e5349d5cca946bc5839f66a201a792
Michal Marhan <marhanmichal@gmail.com>
2023-06-12 14:11:45 +0200

    New backend

--- ios/freshpoint/freshpoint/Network/Fetch.swift
+++ ios/freshpoint/freshpoint/Network/Fetch.swift
@@ -28,7 +28,7 @@ class FoodItemFetcher {
     
     func fetchFridges() async throws -> [Fridge] {
         print("fetching Fridges list")
-        guard let url = URL(string: "/api/freshpoint", relativeTo: Constants.baseURL) else {
+        guard let url = URL(string: "/api/freshpoint/298", relativeTo: Constants.baseURL) else {
             throw NetworkError.badURL
         }
         
